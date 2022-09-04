f8f170779065c9accd3626804c6c997fb40ababe
Lon <usmarinec@gmail.com>
2022-09-03 22:39:14 -0600

    completed welcome component

--- blackjack/src/app/welcome/welcome.component.ts
+++ blackjack/src/app/welcome/welcome.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
-
+  public pageTitle = 'Welcome';
   constructor() { }
 
   ngOnInit(): void {
