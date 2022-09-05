a746a1d7ec21436edd579bdf53985921e112b410
Lon <usmarinec@gmail.com>
2022-09-04 23:13:30 -0600

    added initial shuffle

--- blackjack/src/app/game/game.component.ts
+++ blackjack/src/app/game/game.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ICard } from './game-card';
 import { GameService } from './game-card.service';
 
 @Component({
@@ -10,10 +12,38 @@ export class GameComponent implements OnInit {
   public pageTitle: string = "Game";
   imageWidth: number = 50;
   imageMargin: number = 2;
+  cards:ICard[] = [];
+  shuffledCards:ICard[] = [];
+  errorMessage:string = '';
+  sub!:Subscription;
   
   constructor(private gameService: GameService) { }
 
   ngOnInit(): void {
+    this.sub = this.gameService.getCards().subscribe({
+      next: cards => {
+        this.cards = cards;
+        this.shuffledCards = this.performShuffle(this.cards);
+      },
+      error: err => this.errorMessage = err
+    });
   }
 
+  ngOnDestroy():void {
+    this.sub.unsubscribe();
+  }
+
+  performShuffle(cards:ICard[]):ICard[] {
+    let currentIndex:number = cards.length;
+    let randomIndex!:number;
+
+    while(currentIndex !=0) {
+      randomIndex = Math.floor(Math.random()*currentIndex);
+      currentIndex--;
+
+      [cards[currentIndex], cards[randomIndex]] = [
+        cards[randomIndex], cards[currentIndex]];
+    }
+    return cards;
+  }
 }
